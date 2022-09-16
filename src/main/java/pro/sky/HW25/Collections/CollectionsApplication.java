c4a0d40676e8b200ec94a939eac3e809b638c127
astra <diman.lan@Mail.ru>
2022-09-16 15:30:06 +0300

    First commit

---
+++ src/main/java/pro/sky/HW25/Collections/CollectionsApplication.java
@@ -0,0 +1,14 @@
+package pro.sky.HW25.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+public class CollectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/HW25/Collections/CollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.HW25.Collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
